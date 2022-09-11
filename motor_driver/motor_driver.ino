e7237d445f0c96610507bd6ced00d4bf37df7ea4
Joshua Marangoni <joshua.marangoni@gmail.com>
2022-09-11 16:42:52 -0700

    add HTTP LED control

--- motor_driver/motor_driver.ino
+++ motor_driver/motor_driver.ino
@@ -1,9 +1,28 @@
 
+#include <WiFi.h>
+
 #define IN1 18
 #define IN2 19
 #define IN3 22
 #define IN4 23
 #define LED 32
+
+const char* ssid = "Bubba Wi-fi Josh";
+const char* password = "bubbapassword";
+
+WiFiServer server(80);
+
+String header;
+
+// Auxiliar variables to store the current output state
+String LED_State = "off";
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
 void straight() {
   digitalWrite(IN1, HIGH);
@@ -34,31 +53,128 @@ void off() {
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  // motors
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+  off();
+  Serial.println("Motor Driver Initialized");
+
+  // webserver
   pinMode(LED, OUTPUT);
   digitalWrite(LED, HIGH);
-  Serial.println("Motor Driver Start");
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
 }
 
 void loop() {
-  off();
-  delay(1000);
-  straight();
-  delay(1000);
-  off();
-  delay(1000);
-  left();
-  delay(1000);
-  off();
-  delay(1000);
-  straight();
-  delay(1000);
-  off();
-  delay(1000);
-  right();
-  delay(1000);
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /LED/on") >= 0) {
+              Serial.println("LED on");
+              LED_State = "on";
+              digitalWrite(LED, HIGH);
+            } else if (header.indexOf("GET /LED/off") >= 0) {
+              Serial.println("LED off");
+              LED_State = "off";
+              digitalWrite(LED, LOW);
+            }
+
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Robot Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for LED  
+            client.println("<p>LED - State " + LED_State + "</p>");
+            // If the LED is off, it displays the ON button       
+            if (LED_State=="off") {
+              client.println("<p><a href=\"/LED/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/LED/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+
+            client.println("</body></html>");
+
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+
+//  off();
+//  delay(1000);
+//  straight();
+//  delay(1000);
+//  off();
+//  delay(1000);
+//  left();
+//  delay(1000);
+//  off();
+//  delay(1000);
+//  straight();
+//  delay(1000);
+//  off();
+//  delay(1000);
+//  right();
+//  delay(1000);
 }
