3224bfeeb93be484b6fe1ecb1e530aab3a935850
Joshua Marangoni <joshua.marangoni@gmail.com>
2022-11-01 20:56:35 -0700

    add motor and web server code

--- main/main.ino
+++ main/main.ino
@@ -7,6 +7,63 @@
 
 BluetoothSerial SerialBT;
 
+#include <WiFi.h>
+
+#define IN1 18
+#define IN2 19
+#define IN3 22
+#define IN4 23
+#define LED 32
+
+String Drive_State = "off";
+
+const char* ssid = "";
+const char* password = "";
+
+WiFiServer server(80);
+
+String header;
+
+// Auxiliar variables to store the current output state
+String LED_State = "off";
+
+// Current time
+unsigned long currentTime = millis();
+
+// Previous time
+unsigned long previousTime = 0;
+
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+void straight() {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN4, HIGH);
+    digitalWrite(IN3, LOW);
+}
+
+void left() {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN4, LOW);
+    digitalWrite(IN3, HIGH);
+}
+
+void right() {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN4, HIGH);
+    digitalWrite(IN3, LOW);
+}
+
+void off() {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN4, LOW);
+    digitalWrite(IN3, LOW);
+}
+
 #include <Wire.h>
 #include <ArduCAM.h>
 #include <SPI.h>
@@ -100,6 +157,34 @@ void setup() {
     myCAM.OV2640_set_JPEG_size(OV2640_320x240);
     delay(1000);
     myCAM.clear_fifo_flag();
+
+    // motors
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    off();
+    Serial.println("Motor Driver Initialized");
+
+    // webserver
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, HIGH);
+    // Connect to Wi-Fi network with SSID and password
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    // Print local IP address and start web server
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    server.begin();
 }
 
 void loop() {
@@ -333,6 +418,114 @@ void loop() {
             myCAM.clear_fifo_flag();
         }
     }
+
+    WiFiClient client = server.available();   // Listen for incoming clients
+
+    if (client)
+    {   // If a new client connects,
+        currentTime = millis();
+        previousTime = currentTime;
+        Serial.println("New Client.");          // print a message out in the serial port
+        String currentLine = "";                // make a String to hold incoming data from the client
+
+        while (client.connected() && currentTime - previousTime <= timeoutTime)
+        {
+            currentTime = millis();
+
+            if (client.available())
+            {
+                char c = client.read();             // read a byte, then
+                Serial.write(c);                    // print it out the serial monitor
+                header += c;
+
+                if (c == '\n') {                    // if the byte is a newline character
+                    // if the current line is blank, you got two newline characters in a row.
+                    // that's the end of the client HTTP request, so send a response:
+                    if (currentLine.length() == 0) {
+                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                        // and a content-type so the client knows what's coming, then a blank line:
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println("Connection: close");
+                        client.println();
+
+                        // turns the GPIOs on and off
+                        if (header.indexOf("GET /LED/on") >= 0)
+                        {
+                            Serial.println("LED on");
+                            LED_State = "on";
+                            digitalWrite(LED, HIGH);
+                        }
+                        else if (header.indexOf("GET /LED/off") >= 0)
+                        {
+                            Serial.println("LED off");
+                            LED_State = "off";
+                            digitalWrite(LED, LOW);
+                        }
+                        else if (header.indexOf("GET /drive/straight") >= 0)
+                        {
+                            Serial.println("DRIVE STRAIGHT");
+                            Drive_State = "straight";
+                            straight();
+                        }
+                        else if (header.indexOf("GET /drive/off") >= 0)
+                        {
+                            Serial.println("END DRIVE");
+                            Drive_State = "off";
+                            off();
+                        }
+
+                        // Display the HTML web page
+                        client.println("<!DOCTYPE html><html>");
+                        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                        client.println("<link rel=\"icon\" href=\"data:,\">");
+                        // CSS to style the on/off buttons
+                        client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+                        client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+                        client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+                        client.println(".button2 {background-color: #555555;}</style></head>");
+
+                        // Web Page Heading
+                        client.println("<body><h1>ESP32 Robot Web Server</h1>");
+
+                        // Display current state, and ON/OFF buttons for LED
+                        client.println("<p>LED - State " + LED_State + "</p>");
+                        // If the LED is off, it displays the ON button
+
+                        if (LED_State=="off")
+                        {
+                            client.println("<p><a href=\"/LED/on\"><button class=\"button\">ON</button></a></p>");
+                        }
+                        else
+                        {
+                            client.println("<p><a href=\"/LED/off\"><button class=\"button button2\">OFF</button></a></p>");
+                        }
+                        client.println("<p><a href=\"/drive/straight\"><button class=\"button\">DRIVE</button></a></p>");
+                        client.println("<p><a href=\"/drive/off\"><button class=\"button\">STOP</button></a></p>");
+                        client.println("</body></html>");
+
+                        // The HTTP response ends with another blank line
+                        client.println();
+                        // Break out of the while loop
+                        break;
+                }
+                else
+                {   // if you got a newline, then clear currentLine
+                    currentLine = "";
+                }
+            }
+            else if (c != '\r')
+            {   // if you got anything else but a carriage return character,
+                currentLine += c;      // add it to the end of the currentLine
+            }
+        }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
 }
 
 uint8_t read_fifo_burst(ArduCAM myCAM)
