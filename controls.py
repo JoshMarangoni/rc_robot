27a91eefa5b89337fcd1acecaadb8e4ac7f3d1d1
Joshua Marangoni <joshua.marangoni@gmail.com>
2022-11-07 20:39:07 -0800

    sync get request controls working

--- controls.py
+++ controls.py
@@ -7,14 +7,20 @@ print("RC ROBOT CONTROL TERMINAL")
 
 async def sendRequest(endpoint):
     async with aiohttp.ClientSession() as session:
-        url = f"blah blah blah{endpoint}"
+        url = f"http://192.168.86.83/drive/{endpoint}"
         async with session.get(url) as resp:
             ret = await resp.json
             print(ret)
 
+def sendSyncRequest(endpoint):
+    url = f"http://192.168.86.83/drive/{endpoint}"
+    resp = requests.get(url)
+    print(resp)
+
 while 1:
-    c = input("Enter control (wasd): ")
-    if c in ['w', 'a', 's', 'd']:
-        asyncio.run(sendRequest(c))
+    c = input("Enter control (wasdq): ")
+    if c in ['w', 'a', 's', 'd', 'q']:
+        #asyncio.run(sendRequest(c))
+        sendSyncRequest(c)
     else:
         print("Invalid command")
