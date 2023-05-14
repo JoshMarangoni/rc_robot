866729496df0e8f6a990c754db227f547769bc98
Joshua Marangoni <joshua.marangoni@gmail.com>
2023-05-14 16:51:01 -0700

    arduino control test both motors

--- firmware/arduino/h_bridge_motor_test/h_bridge_motor_test.ino
+++ firmware/arduino/h_bridge_motor_test/h_bridge_motor_test.ino
@@ -2,32 +2,45 @@
 #define M1 25
 #define M2 26
 
+#define M3 19
+#define M4 18
+
 void setup()
 {
     Serial.begin(115200);
     pinMode(M1, OUTPUT);
     pinMode(M2, OUTPUT);
+    pinMode(M3, OUTPUT);
+    pinMode(M4, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(M1, HIGH);
   digitalWrite(M2, LOW);
+  digitalWrite(M3, HIGH);
+  digitalWrite(M4, LOW);
 
   delay(1000);
 
   digitalWrite(M1, LOW);
   digitalWrite(M2, LOW);
+  digitalWrite(M3, LOW);
+  digitalWrite(M4, LOW);
 
   delay(1000);
 
   digitalWrite(M1, LOW);
   digitalWrite(M2, HIGH);
+  digitalWrite(M3, LOW);
+  digitalWrite(M4, HIGH);
 
   delay(1000);
 
   digitalWrite(M1, LOW);
   digitalWrite(M2, LOW);
+  digitalWrite(M3, LOW);
+  digitalWrite(M4, LOW);
 
   delay(1000);
 }
