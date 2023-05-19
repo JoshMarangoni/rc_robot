c4a82e4d393553e1b470c3e8e45fd4c8a3f73e2f
Joshua Marangoni <joshua.marangoni@gmail.com>
2023-05-18 18:49:42 -0700

    full async terminal and async http

--- scripts/controls.py
+++ scripts/controls.py
@@ -2,25 +2,51 @@
 import requests
 import aiohttp
 import asyncio
+import msvcrt
 
-print("RC ROBOT CONTROL TERMINAL")
+ESP32_DRIVE_API = "http://192.168.86.29:80/drive"
 
-async def sendAsyncRequest(endpoint):
+print("###############################################")
+print("########## RC ROBOT CONTROL TERMINAL ##########")
+print("###############################################")
+print("\nUse the WASD keys to control motor direction:")
+
+async def sendAsyncRequest(url):
     async with aiohttp.ClientSession() as session:
-        url = f"http://192.168.86.29:80/drive/{endpoint}"
-        async with session.get(url) as resp:
-            ret = await resp.json
-            print(ret)
+        async with session.get(url) as response:
+            if response.status == 200:
+                return
+            else:
+                print("Request failed with status code:", response.status)
 
-def sendSyncRequest(endpoint):
-    url = f"http://192.168.86.29:80/drive/{endpoint}"
-    resp = requests.get(url)
-    print(resp)
-
-while 1:
-    c = input("Enter control (wasdq): ")
+async def sendRobotCommand(c):
     if c in ['w', 'a', 's', 'd', 'q']:
-        #asyncio.run(sendRequest(c))
-        sendSyncRequest(c)
+        url = f"{ESP32_DRIVE_API}/{c}"
+        try:
+            await sendAsyncRequest(url)
+        except Exception as e:
+            print("An error occurred:", str(e))
     else:
         print("Invalid command")
+
+def main():
+    while True:
+        """
+        Get user input without needing to press enter.
+        """
+        input_chars = []
+
+        if msvcrt.kbhit():
+            char = msvcrt.getch().decode('utf-8')
+            if char == '\x03':  # Break the loop on Ctrl+C (ASCII value 03)
+                raise KeyboardInterrupt
+            else:
+                input_chars.append(char)
+                print(char, end='', flush=True)  # Echo the character back to the console
+                loop = asyncio.get_event_loop()
+                loop.run_until_complete(sendRobotCommand(char))
+
+try:
+    main()
+except KeyboardInterrupt:
+    print("\nExiting.")
